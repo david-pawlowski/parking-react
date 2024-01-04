de0107126c33193ea0a7789424433188a2177fab
dawid-pawlowski <dawid.pawlowski@zielonykod.com>
2024-01-04 01:07:05 +0100

    First react attempt

--- src/App.js
+++ src/App.js
@@ -1,25 +1,63 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+export default function App() {
+  const [spots, setSpots] = React.useState([]);
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>Parking Lot</h1>
       </header>
+      <main>
+        <Parking spots={spots} />
+        <AddParkingSpotForm spots={spots} onSetSpots={setSpots} />
+      </main>
     </div>
   );
 }
 
-export default App;
+function AddParkingSpotForm({ spots, onSetSpots }) {
+  const [spotName, setSpotName] = useState("");
+
+  function handleSubmit(event) {
+    event.preventDefault();
+    if (spotName === "") return;
+
+    onSetSpots((spots) => [...spots, spotName]);
+    setSpotName("");
+  }
+
+  return (
+    <form className="form" onSubmit={handleSubmit}>
+      <input
+        type="text"
+        placeholder="Spot Number"
+        value={spotName}
+        onChange={(e) => setSpotName(e.target.value)}
+      />
+      <button>Add</button>
+    </form>
+  );
+}
+
+function Parking({ spots }) {
+  return (
+    <div className="parking">
+      {spots.map((spot) => (
+        <ParkingSpot key={spot} number={spot} />
+      ))}
+    </div>
+  );
+}
+
+function ParkingSpot({ number }) {
+  const [isSpotTaken, setIsSpotTaken] = useState(false);
+  return (
+    <button
+      className={isSpotTaken ? "taken" : "square"}
+      onClick={() => setIsSpotTaken(!isSpotTaken)}
+    >
+      {number}
+    </button>
+  );
+}
